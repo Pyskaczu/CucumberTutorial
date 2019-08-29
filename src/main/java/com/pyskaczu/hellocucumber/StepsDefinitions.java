96f07c9312871c16e57530cd5ae9c7e378e1bbf5
Michal Babiarz <michal.babiarz84@gmail.com>
2019-08-29 16:36:56 +0200

    Adds tracing to HTTP requests

--- src/main/java/com/pyskaczu/hellocucumber/StepsDefinitions.java
+++ src/main/java/com/pyskaczu/hellocucumber/StepsDefinitions.java
@@ -3,9 +3,16 @@ package com.pyskaczu.hellocucumber;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
 import java.net.URI;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.Assert.*;
 
@@ -31,9 +38,8 @@ public class StepsDefinitions {
 	@When("^GET HTTP to \"([^\"]*)\" is called$")
 	public void get_http_to_is_called(String url) {
 		RestTemplate rt = new RestTemplate();
-		String response = rt.getForObject(URI.create("http://" + url), String.class);
-
-		System.out.println(response);
+		rt.setInterceptors(Stream.of(new Tracer()).collect(Collectors.toList()));
+		rt.getForObject(URI.create("http://" + url), String.class);
 	}
 
 
@@ -41,5 +47,20 @@ public class StepsDefinitions {
 		static String isItFriday(String today) {
 			return "Friday".equals(today) ? "Yes" : "Nope" ;
 		}
+	}
+
+	private static class Tracer implements ClientHttpRequestInterceptor {
+
+		@Override public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+			log("Sending " + request.getMethodValue() + " to " + request.getURI().toString());
+			ClientHttpResponse response = execution.execute(request, body);
+			log("Status code " + response.getStatusCode().toString());
+
+			return response;
+		}
+	}
+
+	public static void log(String message) {
+		System.out.println(message);
 	}
 }
